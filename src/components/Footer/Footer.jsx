c0b1d01dfd63c0d00bf59b28c65fd33ba7234920
Ezequiel <ezgarcia@frba.utn.edu.ar>
2023-10-23 20:48:56 -0300

    terminando footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import Contact from '../Contact/Contact';
 import footerStyle from './Footer.module.css';
+import iconBe from '../../assets/icons-footer/behance.png';
 import iconFace from '../../assets/icons-footer/facebook.png';
+import iconTwitter from '../../assets/icons-footer/gorjeo.png';
 
 export default function Footer(){
     return(
@@ -11,11 +13,16 @@ export default function Footer(){
                     <img className={footerStyle.icon} src={iconFace} alt="facebook" />
                 </div>
                 <div className={footerStyle.contentIcons}>
-                    <img className={footerStyle.icon} src={iconFace} alt="facebook" />
+                    <img className={footerStyle.icon} src={iconTwitter} alt="facebook" />
                 </div>
                 <div className={footerStyle.contentIcons}>
-                    <img className={footerStyle.icon} src={iconFace} alt="facebook" />
+                    <img className={footerStyle.icon} src={iconBe} alt="facebook" />
                 </div>
+            </section>
+            <section className={footerStyle.contentCopy}>
+                <p className={footerStyle.text}>
+                    Copy © 2023 - Todos los derechos reservados | Diseñado y desarrollado por: <a href="#" className={footerStyle.autor}>Ezequiel Garcia</a>
+                </p>
             </section>
         </div>
     )
